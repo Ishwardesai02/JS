fca57fbfb0d1a03cd844f1ac70a009836231eb23
Ishwar Desai <ishwardesai077@gmail.com>
2025-04-12 20:16:13 +0000

    Data Types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"//treat all JS code as newer version
+
+//alert("Hello") //We are using nodejs , not browser
+
+console.log(9+3);
+console.log("Ishwar")
+
+
+let name="Ishwar"
+let age=20
+let isLoggedIn=false
+
+//number => 2^53 range of number
+//bigint 
+//string=>""
+//boolean => true/false
+//null => standalone Value 
+//undefined => 
+// Symbol => unique
+
+//object
+console.log(typeof"ishwar")
+console.log(typeof null)   //type of null is object
