92a896337936e44fa51e3509651160d8a3e06321
Ishwar Desai <ishwardesai077@gmail.com>
2025-04-15 17:00:40 +0000

    Stack and Heap Memory

---
+++ 01_basics/05_memory.js
@@ -0,0 +1,21 @@
+
+
+// Satck(primitive), Heap (Non - primitive)
+
+let myYoutubeName="Ishwardotcom"
+
+let anothername=myYoutubeName
+anothername="chai aur code"
+
+console.log(anothername);
+
+let user1={
+    email:"ishwar@gmail.com",
+    upi:"usery@bl"
+}
+let user2=user1
+user2.email="krishna@gmail.com"
+
+console.log(user1.email);
+console.log(user2.email);
+
