4d80d56bc7caed31e219fa6d052a382a28f6dddc
Ishwar Desai <ishwardesai077@gmail.com>
2025-04-15 16:59:27 +0000

    Data types Updated

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -21,3 +21,24 @@ let isLoggedIn=false
 //object
 console.log(typeof"ishwar")
 console.log(typeof null)   //type of null is object
+
+//Primitive (7) : they are call by value , changes are made in copy
+// String,Number,Boolean,null,undefined,Symbol,Bigint
+
+//
+
+//Non Primitive(reference type) : Referance from memory is directly allocated
+// Array , Objects ,Functions 
+const heros=["shaktimaan","naagraaj","doga"]
+
+let myObj={           //datatype:Object
+    name:"Ishwar",
+    age:22,
+}
+
+const myFunction=function(){  //datatype : function
+    console.log("Hello worls");
+}
+
+console.log(typeof myObj);
+console.log(typeof myFunction);
