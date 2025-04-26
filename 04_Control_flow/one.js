b612305cb7cc2752954a4f03950f39b22550153d
Ishwar Desai <ishwardesai077@gmail.com>
2025-04-26 08:23:48 +0000

    control flow

---
+++ 04_Control_flow/one.js
@@ -0,0 +1,17 @@
+if(true){
+    //if true code will be executed
+}
+
+const isUserLoggedIN=true
+
+if(isUserLoggedIN){
+
+}
+
+const temp=41
+
+if(temp<50){
+    console.log("temp is less then 50")
+}
+console.log("temp is greter then 50")
+
